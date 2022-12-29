9a97a74966d2fe55de2e493d843612bf25a42a06
Yan <yangabriel2012@gmail.com>
2022-12-29 09:24:17 -0300

    feat: adicionando provider do theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux'
-import store from '../redux/store'
+import type { AppProps } from "next/app"
+import { Provider } from "react-redux"
+import store from "../redux/store"
+import { ThemeProvider } from "styled-components"
+import { theme } from "../styles/theme"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <Provider store={store}>
-    <Component {...pageProps} />
-  </Provider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   )
 }
