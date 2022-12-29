231522aa4ce30961617ab92b1f7e02e04587a1de
Yan <yangabriel2012@gmail.com>
2022-12-29 09:23:58 -0300

    feat: criando interface do objeto de theme

---
+++ src/interfaces/themeInterface.ts
@@ -0,0 +1,43 @@
+export interface ThemeInterface{
+      colors?:{
+          green?: string,
+          red?: string,
+          white?: string,       
+          ciano?: string,       
+          blue?: string,       
+          gray?: string,
+          opacityBlack?: string      
+      }
+    
+      fonts: {
+        family?: {
+          default?: string,
+          secondary?: string,
+        },
+        sizes?: {
+          xsmall?: string,
+          small?: string,
+          medium?: string,
+          large?: string,
+          xlarge?: string,
+          xxlarge?: string,
+          huge?: string,
+          xhuge?: string,
+        },
+      },
+      media?: {
+        lteMedium?: string,
+        lteLarge?: string,
+      },
+      spacings?: {
+        xsmall?: string,
+        small?: string,
+        medium?: string,
+        large?: string,
+        xlarge?: string,
+        xxlarge?: string,
+        huge?: string,
+        xhuge?: string,
+      },
+    
+  }  
