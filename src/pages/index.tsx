793174e4078a0b7ca798175bd47191ad2f151429
Yan <yangabriel2012@gmail.com>
2023-01-05 09:29:08 -0300

    fix: atualizando o estado do redux

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import { Form } from "../components/Form"
 import { Content } from "../components/Content"
 import React, { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useTodos } from "../redux/sliceTodos"
+import { useTodos, addTodo } from "../redux/sliceTodos"
 
 export default function Home() {
   const [task, setTask] = useState<string>("")
   const [title, setTitle] = useState<string>("")
   const todos = useSelector(useTodos)
-  console.log(todos)
+  const dispatch = useDispatch() 
 
   const handleChangeTask = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTask(event.target.value)
@@ -17,7 +17,9 @@ export default function Home() {
   const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value)
   }
+  const handleSubimit = () => {
 
+  }
   return (
     <>
       <Content>
@@ -26,6 +28,7 @@ export default function Home() {
           title={title}
           handleChangeTask={handleChangeTask}
           handleChangeTitle={handleChangeTitle}
+          onClick={() => {dispatch(addTodo({title: title, task: task})), console.log(todos)}}
         />
       </Content>
     </>
