dd50f1c05851f4174d9357b603bfaa5a03df43fb
Yan <yangabriel2012@gmail.com>
2023-01-05 09:28:34 -0300

    fix: extendendo a interface do button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,8 @@ import React, {} from "react"
 export interface ButtonInterface extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactNode | string
   color: "green" | "blue" | "red"
+  
 }
-export const Button = ({ children, color = "blue" }: ButtonInterface) => {
-  return <Styled.Button color={color}>{children}</Styled.Button>
+export const Button = ({ children, color = "blue", ...props }: ButtonInterface) => {
+  return <Styled.Button color={color} {...props} >{children}</Styled.Button>
 }
