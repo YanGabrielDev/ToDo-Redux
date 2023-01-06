0a16e95558c2247df32d8000f2087147f246c666
yangabrp <yg@forpeople.io>
2023-01-06 15:27:31 -0300

    feat: implementação do taskContainer

---
+++ src/components/TaskContainer/styled.ts
@@ -0,0 +1,34 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+${({theme}) => css`
+width: 100%;
+padding: 0 24px;
+overflow: hidden;
+border-radius: 24px;
+margin-top: 3rem;
+box-shadow: -1px 0px 5px 7px rgba(0,0,0,0.12);
+
+`}
+`
+export const Title = styled.h2`
+${({theme}) => css`
+color: ${theme.colors.text};
+padding: 20px;
+border: none;
+outline: none;
+margin: 20px;
+width: 90%;
+font-size: 30px;
+`}
+
+`
+export const Task = styled.p`
+padding: 20px;
+border: none;
+outline: none;
+margin: 20px;
+width: 90%;
+font-size: 20px;
+
+`
