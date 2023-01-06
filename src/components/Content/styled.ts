48e3ac734075fdcb90bd5a76f69b297a5acc75f1
yangabrp <yg@forpeople.io>
2023-01-06 15:19:26 -0300

    fix: ajuste visual no content

--- src/components/Content/styled.ts
+++ src/components/Content/styled.ts
@@ -4,13 +4,15 @@ export const Container = styled.div`
 ${({theme}) => css`
 width: 100%;
 max-width: 760px;
+background-color: ${theme.colors.white};
 padding: 0 1rem;
 margin:0 auto;
-height: 100%;
+/* height: 100%; */
+border-radius: 24px;
+margin-top: 5rem;
 display: flex;
 justify-content: center;
 flex-direction: column;
-padding-top: 6rem;
 padding-bottom: 6rem;
 font-family: ${theme.fonts?.family?.default};
 `}
