d3fe0cc53c4473ce254f48a008e4c1333a66b6b2
yangabrp <yg@forpeople.io>
2023-01-06 15:28:31 -0300

    feat: implementando id no todo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,15 @@ import { Form } from "../components/Form"
 import { Content } from "../components/Content"
 import React, { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useTodos, addTodo } from "../redux/sliceTodos"
+import { useTodos, addTodo, ToDo } from "../redux/sliceTodos"
+import { TaskContainer } from "../components/TaskContainer"
 
 export default function Home() {
   const [task, setTask] = useState<string>("")
   const [title, setTitle] = useState<string>("")
-  const todos = useSelector(useTodos)
-  const dispatch = useDispatch() 
-
+  const todos: ToDo[] = useSelector(useTodos)
+  const dispatch = useDispatch()
+  let id = 0
   const handleChangeTask = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTask(event.target.value)
   }
@@ -17,19 +18,36 @@ export default function Home() {
   const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value)
   }
-  const handleSubimit = () => {
-
+  const getId = () => {
+    return id++
+  }
+  const Tasks = (todos: ToDo[]) => {
+    return todos.map((todo) => {
+      return (
+        <>
+          <TaskContainer task={todo.task} title={todo.title} />
+        </>
+      )
+    })
+  }
+  const handleSubmit = () => {
+    dispatch(addTodo({ id: getId(), title: title, task: task })),
+      setTask(""),
+      setTitle("")
   }
   return (
     <>
       <Content>
-        <Form
-          task={task}
-          title={title}
-          handleChangeTask={handleChangeTask}
-          handleChangeTitle={handleChangeTitle}
-          onClick={() => {dispatch(addTodo({title: title, task: task})), console.log(todos)}}
-        />
+        <>
+          <Form
+            task={task}
+            title={title}
+            handleChangeTask={handleChangeTask}
+            handleChangeTitle={handleChangeTitle}
+            onClick={handleSubmit}
+          />
+          {Tasks(todos)}
+        </>
       </Content>
     </>
   )
