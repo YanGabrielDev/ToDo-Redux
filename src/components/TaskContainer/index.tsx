2c7d9a68038143f1b05143c250fc75ae2d559fd3
Yan <yangabriel2012@gmail.com>
2023-01-07 23:03:43 -0300

    feat: implementando função de delete

--- src/components/TaskContainer/index.tsx
+++ src/components/TaskContainer/index.tsx
@@ -1,13 +1,21 @@
 import * as Styled from "./styled"
+import {ButtonContainer} from '../Form/styled'
+import { Button } from "../Button"
+import { MouseEventHandler } from "react"
+import { deleteTodo } from "../../redux/sliceTodos"
 interface TaskContainer {
   title: string
   task: string
+  deleteTodo:  MouseEventHandler<HTMLButtonElement>
 }
-export const TaskContainer = ({ task, title }: TaskContainer) => {
+export const TaskContainer = ({ task, title, deleteTodo }: TaskContainer) => {
   return (
     <Styled.Container>
       <Styled.Title>{title}</Styled.Title>
       <Styled.Task>{task}</Styled.Task>
+      <ButtonContainer>
+        <Button onClick={deleteTodo} color='red'>Deletar</Button>
+      </ButtonContainer>
     </Styled.Container>
   )
 }
