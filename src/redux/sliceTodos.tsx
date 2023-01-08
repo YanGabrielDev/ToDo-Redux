201047ecd327f0ffd40834f3cf085cf7d0207559
Yan <yangabriel2012@gmail.com>
2023-01-07 23:03:59 -0300

    feat: implementando função de delete

--- src/redux/sliceTodos.tsx
+++ src/redux/sliceTodos.tsx
@@ -6,21 +6,31 @@ export interface ToDo {
   task: string
 }
 
-const INITIAL_STATE: ToDo[] = []
+const INITIAL_STATE: ToDo[] = [
+  
+]
 
 const sliceTodos = createSlice({
   name: "Todos",
   initialState: INITIAL_STATE,
   reducers: {
     addTodo(state, action) {
-      state.push(action.payload)
+    state.push(
+      action.payload 
+    ) 
+    },
+    deleteTodo(state, action) {
+   let opa = state.filter((todo, index) => action.payload !== index)
+     return opa;
+     
+     
     },
   },
 })
 
 export default sliceTodos.reducer
 
-export const { addTodo } = sliceTodos.actions
+export const { addTodo, deleteTodo } = sliceTodos.actions
 
 export const useTodos = (state: any) => {
   return state.todos as ToDo[]
