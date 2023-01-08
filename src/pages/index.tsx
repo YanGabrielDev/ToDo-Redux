79d10c15851a3f7da8b965573856724c9aeae50d
Yan <yangabriel2012@gmail.com>
2023-01-07 23:03:50 -0300

    feat: implementando função de delete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Form } from "../components/Form"
 import { Content } from "../components/Content"
 import React, { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useTodos, addTodo, ToDo } from "../redux/sliceTodos"
+import { useTodos, addTodo, ToDo, deleteTodo } from "../redux/sliceTodos"
 import { TaskContainer } from "../components/TaskContainer"
 
 export default function Home() {
@@ -10,30 +10,37 @@ export default function Home() {
   const [title, setTitle] = useState<string>("")
   const todos: ToDo[] = useSelector(useTodos)
   const dispatch = useDispatch()
-  let id = 0
   const handleChangeTask = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTask(event.target.value)
   }
-
+  
   const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value)
   }
+  let id: number = 0
   const getId = () => {
-    return id++
+    return ++id ;
+    
+  }
+  
+  const handleSubmit = () => {
+    dispatch(addTodo({ id: id , title: title, task: task })),
+      setTask(""),
+      setTitle("")
   }
   const Tasks = (todos: ToDo[]) => {
-    return todos.map((todo) => {
+    return todos.map((todo, index) => {
       return (
         <>
-          <TaskContainer task={todo.task} title={todo.title} />
+          <TaskContainer
+            key={todo.id}
+            task={todo.task}
+            title={todo.title}
+            deleteTodo={() => {dispatch(deleteTodo(index)), console.log(todos)}}
+          />
         </>
       )
     })
-  }
-  const handleSubmit = () => {
-    dispatch(addTodo({ id: getId(), title: title, task: task })),
-      setTask(""),
-      setTitle("")
   }
   return (
     <>
