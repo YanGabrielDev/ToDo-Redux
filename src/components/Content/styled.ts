8dbfc97eee3cf5b6ac976f9a254b14286823baab
Yan <yangabriel2012@gmail.com>
2023-01-10 22:38:06 -0300

    fix: correção de estilo

--- src/components/Content/styled.ts
+++ src/components/Content/styled.ts
@@ -4,12 +4,12 @@ export const Container = styled.div`
 ${({theme}) => css`
 width: 100%;
 max-width: 760px;
-background-color: ${theme.colors.white};
+background-color: ${theme.colors.softBlack2};
+
 padding: 0 1rem;
 margin:0 auto;
 /* height: 100%; */
 border-radius: 24px;
-margin-top: 5rem;
 display: flex;
 justify-content: center;
 flex-direction: column;
