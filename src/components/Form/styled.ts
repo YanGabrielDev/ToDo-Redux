31157b4728cac24b5cb5e3a9970f7ef09964e034
Yan <yangabriel2012@gmail.com>
2023-01-10 22:38:30 -0300

    fix: correção no espaçamento

--- src/components/Form/styled.ts
+++ src/components/Form/styled.ts
@@ -7,29 +7,37 @@ box-shadow: -1px 0px 5px 7px rgba(0,0,0,0.12);
 height: auto;
 border-radius: 24px;
 padding: 0 24px;
+margin-top: 5rem;
 overflow: hidden;
-background-color: ${theme.colors.white};
+background-color: ${theme.colors.softBlack2};
+
 `}
 `
 export const Title = styled.input`
 ${({theme}) => css`
-color: ${theme.colors.text};
+color: white;
+
 padding: 20px;
 border: none;
 outline: none;
 margin: 20px;
 width: 90%;
+background-color: ${theme.colors?.softBlack2};
+
 font-size: 30px;
 `}
 
 `
 export const Task = styled.textarea`
+${({theme}) => css`
 padding: 20px;
 border: none;
 outline: none;
 margin: 20px;
 width: 90%;
+background-color: ${theme.colors?.softBlack2};
 font-size: 20px;
+`}
 
 `
 export const ButtonContainer = styled.div`
