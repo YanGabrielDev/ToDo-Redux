b66af5c35b61c80d0e91ff50f75114534d5f1d9c
Yan <yangabriel2012@gmail.com>
2023-01-10 22:39:31 -0300

    feat: acrescentando as cores do layout

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,13 +1,15 @@
 import { createGlobalStyle } from "styled-components";
-
+import {theme} from './theme'
 export const GlobalStyles = createGlobalStyle`
 *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    color: white;
 }
 body{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    background:  rgba(1,97,204,1)}`
+    background: ${theme.colors?.softBlack}}
+    `
