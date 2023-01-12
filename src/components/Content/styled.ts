c8614cd494c4cfed8b770bdbd2aa39988d61141c
Yan <yangabriel2012@gmail.com>
2023-01-11 22:23:53 -0300

    fix: estilização do content

--- src/components/Content/styled.ts
+++ src/components/Content/styled.ts
@@ -5,12 +5,11 @@ ${({theme}) => css`
 width: 100%;
 max-width: 760px;
 background-color: ${theme.colors.softBlack2};
-
 padding: 0 1rem;
 margin:0 auto;
-/* height: 100%; */
 border-radius: 24px;
 display: flex;
+margin-top: 5px;
 justify-content: center;
 flex-direction: column;
 padding-bottom: 6rem;
